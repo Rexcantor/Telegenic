06879b78ff146531a473329d2a638a597b1be0f5
Diogo Correia <diogo.correia2002@gmail.com>
2018-02-04 16:36:05 +0000

    Started creating the image parser.

--- renderer.js
+++ renderer.js
@@ -1,3 +1,5 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+var telegenic = require("./lib/telegenic.js");
+document.getElementById("start").onclick = function() { telegenic.makeSlideshow(); };
